c0d11ffb11361dc4201af23d0e57daf026821aff
morganegosse <70743930+morganegosse@users.noreply.github.com>
2020-12-04 15:11:03 +0100

    Add files via upload

---
+++ phpexostitouan/index.php
@@ -0,0 +1,20 @@
+<?php
+
+
+$userName=0;
+
+if($userName<0){
+    echo"nmbre negatif";
+} elseif($userName>0){
+    echo"nombre positif";
+}
+elseif($userName===0){
+    echo"egale à 0";
+}
+
+
+/*   === : prendre en compte le type(nbre ou texte)
+     == : uniquement comparer la valeure
+*/
+
+
